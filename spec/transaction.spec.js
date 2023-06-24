96836f83003ac07b695bdfd17a0ef83ad763fc96
Oana <oana.demian@gmail.com>
2023-06-24 15:14:47 +0100

    should add another 2000 to the balance when a deposit transaction is made

--- spec/transaction.spec.js
+++ spec/transaction.spec.js
@@ -11,4 +11,16 @@ describe('Transaction Tests', () => {
     //Assert
     expect(transaction.getBalance()).toBe(1000.00);
   })
+
+    it('should add another 2000 to the balance when a deposit transaction is made', () => {
+    //Arrange
+    let initialBalance = 0;
+    let expected = 3000.00;
+    let transaction = new Transaction(initialBalance);
+    //Act
+      transaction.depositSum(1000);
+      transaction.depositSum(2000);
+    //Assert
+    expect(transaction.getBalance()).toBe(3000.00);
+  })
 })
