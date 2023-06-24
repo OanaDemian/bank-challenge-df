5a6933f936cc6530183b1df2dee1f2f233d7ec3e
Oana <oana.demian@gmail.com>
2023-06-24 20:38:51 +0100

    should call getTransactionDate on the transaction object when getTransactionDate is called on the account

--- spec/account.spec.js
+++ spec/account.spec.js
@@ -4,54 +4,64 @@ class MockTransaction {
   depositSum = sumToDeposit => { }
   withdrawSum = sumToWithdraw => { }
   getBalance = () => 1000.00
+  getTransactionDate = () => '11.01.2012'
 }
 
 describe('Account Tests', () => {
 
-    let account, transaction;
+  let account, transaction;
 
-    beforeEach(() => {
-        transaction = new MockTransaction();
-        account = new Account(transaction);
-    });
+  beforeEach(() => {
+    transaction = new MockTransaction();
+    account = new Account(transaction);
+  });
 
-    afterEach(() => {
-        transaction = undefined;
-        account = undefined;
-    });
+  afterEach(() => {
+    transaction = undefined;
+    account = undefined;
+  });
     
-    it('should call depositSum on the transaction object when credit is called on the account', () => {
+  it('should call depositSum on the transaction object when credit is called on the account', () => {
         
-        // Arrange
-        const transactionSpy = spyOn(transaction, `depositSum`)
-        const depositSum = 1000.00;
+    // Arrange
+    const transactionSpy = spyOn(transaction, `depositSum`)
+    const depositSum = 1000.00;
       
-        // Act
-        account.credit(depositSum);
+    // Act
+    account.credit(depositSum);
 
-        // Assert
-        expect(transactionSpy).toHaveBeenCalledOnceWith(depositSum);
-    });
+    // Assert
+    expect(transactionSpy).toHaveBeenCalledOnceWith(depositSum);
+  });
   
-    it('should call withdrawSum on the transaction object when debit is called on the account', () => { 
-      // Arrange
-      const transactionSpy = spyOn(transaction, `withdrawSum`)
-      const withdrawSum = 500.00;
+  it('should call withdrawSum on the transaction object when debit is called on the account', () => {
+    // Arrange
+    const transactionSpy = spyOn(transaction, `withdrawSum`)
+    const withdrawSum = 500.00;
       
-        // Act
-        account.debit(withdrawSum);
+    // Act
+    account.debit(withdrawSum);
 
-        // Assert
-        expect(transactionSpy).toHaveBeenCalledOnceWith(withdrawSum);
-    });
+    // Assert
+    expect(transactionSpy).toHaveBeenCalledOnceWith(withdrawSum);
+  });
   
-      it('should call getBalance on the transaction object when getBalance is called on the account', () => {
-        // Arrange
-        const transactionSpy = spyOn(transaction, `getBalance`);
-        // Act
-        account.getBalance();
+  it('should call getBalance on the transaction object when getBalance is called on the account', () => {
+    // Arrange
+    const transactionSpy = spyOn(transaction, `getBalance`);
+    // Act
+    account.getBalance();
 
-        // Assert
-        expect(transactionSpy).toHaveBeenCalledTimes(1);
-    });
+    // Assert
+    expect(transactionSpy).toHaveBeenCalledTimes(1);
+  });
+  
+  it('should call getTransactionDate on the transaction object when getTransactionDate is called on the account', () => {
+    // Arrange
+    const transactionSpy = spyOn(transaction, `getTransactionDate`);
+    // Act
+    account.getTransactionDate();
+    // Assert
+    expect(transactionSpy).toHaveBeenCalledTimes(1);
+  });
 });
