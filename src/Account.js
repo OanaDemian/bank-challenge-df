e61d198dccd4ba17c74e6106e7e93fd70924e462
Oana <oana.demian@gmail.com>
2023-06-28 12:30:35 +0100

    updates privateBankAccpuntChecks to check the data types of amount and type

--- src/Account.js
+++ src/Account.js
@@ -17,6 +17,12 @@ class Account {
     return this.#newBalance;
   }
 
+  #privateBankAccountChecks(transaction) {
+    if (transaction.getAmount() < 0) throw new Error('Error: Transaction amount must be greater than 0.'); 
+    if ((typeof (transaction.getAmount())) != 'number') throw new Error('Error: Transaction amount must be a number.');
+    if ((typeof (transaction.getType())) != 'string') throw new Error('Error: Transaction type must be a string.');
+  }
+
   #privateDeposit() {
     this.#newBalance = this.#newBalance + this.#transaction.getAmount();
     this.#transactions = [...this.#transactions, { transaction: this.#transaction, newBalance: this.#newBalance}];  
@@ -28,6 +34,7 @@ class Account {
   }
 
   newTransaction(transaction) {
+    this.#privateBankAccountChecks(transaction);
     this.#transaction = transaction;
     if (this.#transaction.getType() === 'credit') this.#privateDeposit();
     if (this.#transaction.getType() === 'debit')  this.#privateWithdrawal();
