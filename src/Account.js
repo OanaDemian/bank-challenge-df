745a33cfd1ca2e2b13609e6fec260250460bff5b
Oana <oana.demian@gmail.com>
2023-07-01 11:09:49 +0100

    adds enum  TnxType class for the credit and debit transaction types

--- src/Account.js
+++ src/Account.js
@@ -18,20 +18,29 @@ class Account {
   }
 
   #privateBankAccountChecks(transaction) {
-    this.#privatePositiveTransactionAmountCheck(transaction); 
-    if ((typeof (transaction.getAmount())) != "number") throw new Error("Error: Transaction amount must be a number.");
-    if ((typeof (transaction.getType())) != "string") throw new Error("Error: Transaction type must be a string.");
+    this.#privateAmountChecks(transaction);
+    // this.#privateTypeChecks(transaction);
+    this.#privateAmountPositiveChecks(transaction);
   }
 
-  #privatePositiveTransactionAmountCheck(transaction) {
+  #privateAmountPositiveChecks(transaction) {
     if (transaction.getAmount() < 0) {
-      throw new Error(
-        "Error: Transaction amount must be greater than 0."
-      );
+      throw new Error("Error: Transaction amount must be greater than 0.");
     }
   }
 
-  
+  #privateAmountChecks(transaction) {
+    if (typeof (transaction.getAmount()) != "number") {
+      throw new Error("Error: Transaction amount must be a number.");
+    }
+  }
+
+  // #privateTypeChecks(transaction) {
+  //   if (typeof (transaction.getType()) != "string") {
+  //     throw new Error("Error: Transaction type must be a string.");
+  //   }
+  // }
+
   #privateDeposit() {
     this.#newBalance = this.#newBalance + this.#transaction.getAmount();
     this.#transactions = [...this.#transactions, { transaction: this.#transaction, newBalance: this.#newBalance}];  
@@ -43,8 +52,8 @@ class Account {
   }
 
   newTransaction(transaction) {
-    this.#privateBankAccountChecks(transaction);
     this.#transaction = transaction;
+    this.#privateBankAccountChecks(transaction);
     if (this.#transaction.getType() === "credit") this.#privateDeposit();
     if (this.#transaction.getType() === "debit")  this.#privateWithdrawal();
   }
