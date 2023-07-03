927d13b6a96d30c34b30ce7393616c6d97e1fa2f
Oana <oana.demian@gmail.com>
2023-07-03 03:24:39 +0100

    refactors index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import Account from "./Account.js";
 import Transaction from "./Transaction.js";
 import TxnType from "./TxnType.js";
+import PrintStatement from "./PrintStatement.js";
 
-const tnxType = new TxnType("credit");
-const transaction1 = new Transaction(new Date(2012, 1, 10), tnxType.type, 1000);
-const transaction2 = new Transaction(new Date(2012, 1, 13), "credit", 2000);
-const transaction3 = new Transaction(new Date(2012, 1, 14), "debit", 500);
+const transaction1 = new Transaction(new Date(2012, 0, 10), TxnType.credit, 1000);
+const transaction2 = new Transaction(new Date(2012, 0, 13), TxnType.credit, 2000);
+const transaction3 = new Transaction(new Date(2012, 0, 14), TxnType.debit, 500);
 const account = new Account();
 account.newTransaction(transaction1);
 account.newTransaction(transaction2);
 account.newTransaction(transaction3);
-console.log(account.getBalance()); 
-console.log(transaction1.getFormattedDate());
-console.log(transaction1.getType());
+console.log(PrintStatement.printTransactionsRows(account.getAccountUpdate()));
